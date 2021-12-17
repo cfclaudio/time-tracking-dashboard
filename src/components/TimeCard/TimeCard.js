ca9108abe814c122cf25405a67e9a2db857e870c
cfclaudio <claudio.charmaine@gmail.com>
2021-12-17 01:11:11 -0500

    Add time card background

--- src/components/TimeCard/TimeCard.js
+++ src/components/TimeCard/TimeCard.js
@@ -1,31 +1,35 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Card, Heading, Text, IconButton, Image } from "theme-ui";
+import { Card, Heading, Text, IconButton, Image, Box } from "theme-ui";
 
-const TimeCard = ({ title, current, previous }) => {
+const TimeCard = ({ title, current, previous, color, icon }) => {
   return (
-    <Card
-      variant="time"
-      sx={{
-        display: "grid",
-        gridTemplateColumns: "1fr 1fr",
-        alignItems: "center",
-      }}
-    >
-      <Heading variant="time.heading">{title}</Heading>
-      <IconButton variant="icon" sx={{ justifySelf: "end" }}>
-        <Image src="icon-ellipsis.svg" />
-      </IconButton>
-      <Text variant="time.current">{current}hrs</Text>
-      <Text sx={{ textAlign: "right" }} variant="time.previous">
-        Last Week - {previous}hrs
-      </Text>
-    </Card>
+    <Box className="time-card" sx={{ position: "relative", my: "4em" }}>
+      <Card variant="timeBackground" bg={color}>
+        <Image
+          src={icon}
+          sx={{ position: "absolute", right: 15, top: "-8px" }}
+        />
+      </Card>
+
+      <Card variant="time">
+        <Heading variant="time.heading">{title}</Heading>
+        <IconButton variant="icon" sx={{ justifySelf: "end" }}>
+          <Image src="icon-ellipsis.svg" />
+        </IconButton>
+        <Text variant="time.current">{current}hrs</Text>
+        <Text sx={{ textAlign: "right" }} variant="time.previous">
+          Last Week - {previous}hrs
+        </Text>
+      </Card>
+    </Box>
   );
 };
 
 TimeCard.propTypes = {
+  color: PropTypes.string.isRequired,
   current: PropTypes.number.isRequired,
+  icon: PropTypes.string.isRequired,
   previous: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
 };
