45dd30b6da762ed3cc6d99491b37ce0707ede9de
cfclaudio <claudio.charmaine@gmail.com>
2021-12-17 00:40:14 -0500

    Move typography fields

---
+++ src/theme/typography.js
@@ -0,0 +1,42 @@
+export const fonts = {
+  body: "Rubik, sans-serif",
+  heading: "Rubik, sans-serif",
+};
+
+export const fontWeights = {
+  bold: 500,
+  normal: 400,
+  light: 300,
+};
+
+export const text = {
+  paragraph: {
+    fontFamily: "body",
+    fontSize: 18,
+  },
+  heading: {
+    fontFamily: "heading",
+    fontSize: 20,
+  },
+  time: {
+    heading: {
+      fontFamily: "heading",
+      fontSize: 18,
+      fontWeight: "bold",
+      lineHeight: "21.33px",
+    },
+    current: {
+      fontFamily: "body",
+      fontSize: 32,
+      fontWeight: "light",
+      lineHeight: "37.92px",
+    },
+    previous: {
+      fontFamily: "body",
+      fontSize: 15,
+      fontWeight: "normal",
+      lineHeight: "17.78px",
+      color: "#BBC0FF",
+    },
+  },
+};
