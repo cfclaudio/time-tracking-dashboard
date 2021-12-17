489b570dcdaf020795cacaa528872f2daf422c05
cfclaudio <claudio.charmaine@gmail.com>
2021-12-16 22:50:23 -0500

    Initial commit

---
+++ theme/index.js
@@ -0,0 +1,12 @@
+export default {
+  fonts: {
+    body: "system-ui, sans-serif",
+    heading: '"Avenir Next", sans-serif',
+    monospace: "Menlo, monospace",
+  },
+  colors: {
+    text: "#fff",
+    background: "#0E1323",
+    primary: "#33e",
+  },
+};
